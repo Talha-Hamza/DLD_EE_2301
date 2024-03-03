00701f301ecfe85d8ed72039a86715d5927d0601
Talha Hamza <hamza050@umn.edu>
2024-03-03 17:40:57 -0600

    lab 3 completed

--- lab3/traffic_controller/project_3.srcs/sources_1/new/TrafficLightController.v
+++ lab3/traffic_controller/project_3.srcs/sources_1/new/TrafficLightController.v
@@ -4,11 +4,17 @@
 module TrafficLightController(
     
     output G1, G2, Y1, Y2, R1, R2,
+    output [2:0] led,
     
     input C1,
     input C2,
     input C0
     );
+    
+    assign led[0] = C0;
+    assign led[1] = C1;
+    assign led[2] = C2;
+
     
     assign G1 = ~(C2)&(C0^C1); // Green light 1
     assign G2 = (C2)&(C0^C1); // Green light 2
